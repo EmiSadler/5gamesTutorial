4d29d7e9aa84cfeb40e6c682e148d1325445d9b8
Emily Sadler <emily@sadler.gg>
2025-02-09 18:37:20 +0000

    added clock to control framerate

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -8,6 +8,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100,200))
@@ -19,20 +20,20 @@ y = 150
 star_surface = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
 
-# importing an image to surface
+#imports
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = 1
 
-# import a meteor
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
-meteor_rect = meteor_surf.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
-# import laser
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
-laser_rect = laser_surf.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
+# Game loop
 while running:
-    # event loop
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -41,12 +42,14 @@ while running:
     display_surface.fill('black')
     for pos in star_positions:
         display_surface.blit(star_surface, pos)
-    if player_rect.right < WINDOW_WIDTH:
-        player_rect.left += 0.2
-    elif player_rect.left > WINDOW_WIDTH:
-        player_rect.right += 0.2
-    display_surface.blit(player_surf, player_rect)
     display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+    
+    # player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 
 
